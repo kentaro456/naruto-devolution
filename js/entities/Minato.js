ad1b871cd6cd31d3a3d9df603ffe1d96303b77bf
kentaro456 <118226803+kentaro456@users.noreply.github.com>
2026-03-11 19:31:34 +0100

    chore: globally slow down Minato attack durations by 40-50%

--- js/entities/Minato.js
+++ js/entities/Minato.js
@@ -18,9 +18,9 @@ class MinatoFighter extends Fighter {
         });
 
         this.attacks = {
-            light: { damage: 7, range: 38, hitHeight: 30, duration: 18, chakraCost: 0, knockback: 2.8, offsetY: -10 },
-            heavy: { damage: 14, range: 48, hitHeight: 34, duration: 28, chakraCost: 0, knockback: 5.6, offsetY: -8 },
-            special: { damage: 28, range: 78, hitHeight: 42, duration: 44, chakraCost: 35, knockback: 13, offsetY: -12, moveSpeed: 5.5, name: 'RASENGAN' },
+            light: { damage: 7, range: 38, hitHeight: 30, duration: 26, chakraCost: 0, knockback: 2.8, offsetY: -10 },
+            heavy: { damage: 14, range: 48, hitHeight: 34, duration: 40, chakraCost: 0, knockback: 5.6, offsetY: -8 },
+            special: { damage: 28, range: 78, hitHeight: 42, duration: 60, chakraCost: 35, knockback: 13, offsetY: -12, moveSpeed: 5.5, name: 'RASENGAN' },
         };
 
         this.specialStyles = {
@@ -29,7 +29,7 @@ class MinatoFighter extends Fighter {
                 name: 'RASENGAN',
                 state: 'SPECIAL',
                 profileOverrides: {
-                    duration: 44,
+                    duration: 60,
                     damageMultiplier: 1.0,
                     rangeMultiplier: 1.0,
                     knockbackMultiplier: 1.0,
@@ -40,7 +40,7 @@ class MinatoFighter extends Fighter {
                 name: 'HIRAISHIN',
                 state: 'SPECIAL_TRANSFORM',
                 profileOverrides: {
-                    duration: 34,
+                    duration: 46,
                     damageMultiplier: 1.05,
                     rangeMultiplier: 1.15,
                     knockbackMultiplier: 1.1,
@@ -51,7 +51,7 @@ class MinatoFighter extends Fighter {
                 name: 'FLASH STEP',
                 state: 'ATTACK_HEAVY_2',
                 profileOverrides: {
-                    duration: 28,
+                    duration: 40,
                     damageMultiplier: 1.2,
                     rangeMultiplier: 1.15,
                     knockbackMultiplier: 1.25,
@@ -62,39 +62,39 @@ class MinatoFighter extends Fighter {
 
         this.comboChains = {
             light: [
-                { state: 'ATTACK_LIGHT_1', duration: 18, cancelWindow: [10, 16] },
-                { state: 'ATTACK_LIGHT_2', duration: 20, cancelWindow: [12, 18] },
-                { state: 'ATTACK_LIGHT_3', duration: 24, cancelWindow: [14, 20] },
+                { state: 'ATTACK_LIGHT_1', duration: 26, cancelWindow: [14, 22] },
+                { state: 'ATTACK_LIGHT_2', duration: 28, cancelWindow: [16, 24] },
+                { state: 'ATTACK_LIGHT_3', duration: 34, cancelWindow: [20, 28] },
             ],
             heavy: [
-                { state: 'ATTACK_HEAVY_1', duration: 28, cancelWindow: [18, 24] },
-                { state: 'ATTACK_HEAVY_2', duration: 34, cancelWindow: [22, 28] },
+                { state: 'ATTACK_HEAVY_1', duration: 40, cancelWindow: [26, 34] },
+                { state: 'ATTACK_HEAVY_2', duration: 48, cancelWindow: [32, 40] },
             ],
             special: [
-                { state: 'SPECIAL', duration: 44 },
+                { state: 'SPECIAL', duration: 60 },
             ],
         };
 
         this.comboCancelRatio = 0.58;
-        this.comboHitResetFrames = 38;
+        this.comboHitResetFrames = 45;
         this._setComboRootRoute('heavy', { forward: 'H_N_1', back: 'H_N_1' });
         this._patchComboNode('L_N_1', {
-            profileOverrides: { state: 'ATTACK_LIGHT_1', duration: 18, damageMultiplier: 1.0 },
+            profileOverrides: { state: 'ATTACK_LIGHT_1', duration: 26, damageMultiplier: 1.0 },
         });
         this._patchComboNode('L_N_2', {
-            profileOverrides: { state: 'ATTACK_LIGHT_2', duration: 20, damageMultiplier: 1.08 },
+            profileOverrides: { state: 'ATTACK_LIGHT_2', duration: 28, damageMultiplier: 1.08 },
             routes: { heavy: { neutral: 'H_N_1', forward: 'H_N_1' } },
         });
         this._patchComboNode('L_N_3', {
-            profileOverrides: { state: 'ATTACK_LIGHT_3', duration: 24, damageMultiplier: 1.2, knockbackMultiplier: 1.15 },
+            profileOverrides: { state: 'ATTACK_LIGHT_3', duration: 34, damageMultiplier: 1.2, knockbackMultiplier: 1.15 },
             routes: { special: { any: 'S_1' } },
         });
         this._patchComboNode('H_N_1', {
-            profileOverrides: { state: 'ATTACK_HEAVY_1', duration: 28, damageMultiplier: 1.2, knockbackMultiplier: 1.2 },
+            profileOverrides: { state: 'ATTACK_HEAVY_1', duration: 40, damageMultiplier: 1.2, knockbackMultiplier: 1.2 },
             routes: { heavy: { neutral: 'H_N_2' }, special: { any: 'S_1' } },
         });
         this._patchComboNode('H_N_2', {
-            profileOverrides: { state: 'ATTACK_HEAVY_2', duration: 34, damageMultiplier: 1.45, knockbackMultiplier: 1.5 },
+            profileOverrides: { state: 'ATTACK_HEAVY_2', duration: 48, damageMultiplier: 1.45, knockbackMultiplier: 1.5 },
             routes: { special: { any: 'S_1' } },
         });
     }
